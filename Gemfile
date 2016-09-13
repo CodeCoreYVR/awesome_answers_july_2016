d4019610f6fcc806e2cac65f186cb8fa44f8328f
Tammam Kbeili <tammam.kbeili@gmail.com>
2016-09-13 15:39:07 -0700

    enable uploading to S3 from CarrierWave

--- Gemfile
+++ Gemfile
@@ -31,6 +31,7 @@ gem 'simple_form'
 gem 'friendly_id'
 
 gem 'carrierwave', '>= 1.0.0.beta', '< 2.0'
+gem "fog"
 gem "mini_magick"
 
 gem 'rack-cors', :require => 'rack/cors'
---
+++ config/initializers/setup_fog.rb
@@ -0,0 +1,11 @@
+CarrierWave.configure do |config|
+  config.fog_credentials = {
+    provider:              'AWS',
+    aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
+    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+    region:                'us-west-2'
+  }
+  config.fog_directory  = 'awesome-answers-july-2016'
+  config.fog_public     = false                                        # optional, defaults to true
+  config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" } # optional, defaults to {}
+end
